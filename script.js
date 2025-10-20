db4b43820b8f15bdff1af41d84d2ca8618940011
Maria Eduarda <mria.mirandadev@gmail.com>
2025-10-20 10:45:26 -0300

    UPDATE: Feature 1 - Adicionando botão que deixa primeira letra maiúscula no HTML, estilizando-o no CSS e deixando-o funcional no JS

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 window.document.addEventListener("DOMContentLoaded", function(){
     // Botão: Letra Maiúscula
     window.document.querySelector("#btnMaiuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toUpperCase()
-    })
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
 
     // Botão: Letra Minúscula
     window.document.querySelector("#btnMinuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toLowerCase()
-    })
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
+
+    window.document.querySelector("#btnCapitalize").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
+    });
 });
